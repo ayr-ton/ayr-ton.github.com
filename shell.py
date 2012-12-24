ab1b45cfe5420db64e63b13aaf383a00ddf2ad47
Ayrton Araujo <root@ayrtonaraujo.net>
2012-12-24 05:46:38 -0400

    terminal

---
+++ shell.py
@@ -0,0 +1,22 @@
+import webapp2
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.out.write("""
+        <!DOCTYPE HTML>
+        <html>
+        <head>
+        <title>ayrtonaraujo.net - v0.3.0 Alpha</title>
+        <link href="css/jquery.terminal.css" rel="stylesheet"/>
+        <script type="text/javascript" src="js/shell-0.3.0.js" async="async">
+        </script>
+        </head>
+        <body>
+        </body>
+        </html>
+        """)
+
+app = webapp2.WSGIApplication([('/', MainPage)],
+                              debug=True)
