77a78b80b6d5ace09394f8e3da483bd8a3771670
Ayrton Araujo <root@ayrtonaraujo.net>
2013-01-01 06:45:06 -0400

    Fixed #25

---
+++ assets/_src/Makefile.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# This script is used to compress all js groups of this projects
+
+# Versions of files
+libs_version=0001
+app_version=0001
+
+# All javascripts of site in their respective groups
+libs=('jquery-1.7.1.js' 'jquery.mousewheel-min.js' 'jquery.terminal-0.4.17.js')
+app=('terminal.js')
+
+# Don't touch in the lines starting from here
+
+main() {
+    if [ -d /tmp/jscache ];
+    then
+        rm -rf /tmp/jscache
+        mkdir /tmp/jscache
+    else
+        mkdir /tmp/jscache
+    fi
+
+    clear
+    echo ""
+    echo "Choose an option:"
+    echo ""
+    echo "1) Minify libs"
+    echo "2) Minify app"
+    echo "3) Minify all"
+    echo "4) Exit."
+    echo -ne "\n: "
+    read op
+
+   case $op in
+      1) clearjs 'libs-*.js'
+            for javascript in ${libs[@]}; do
+                genjs $javascript libs-$libs_version.js
+            done;;
+      2) clearjs 'app-*.js'
+            for javascript in ${app[@]}; do
+                genjs $javascript app-$app_version.js
+            done;;
+      3) clearjs 'libs-*.js'
+            for javascript in ${libs[@]}; do
+                genjs $javascript libs-$libs_version.js
+            done
+         clearjs 'app-*.js'
+            for javascript in ${app[@]}; do
+                genjs $javascript app-$app_version.js
+            done;;
+      4) exit ;;
+      *) "Unknown option." ; echo ; main ;;
+   esac
+}
+
+clearjs() {
+    rm ../js/$1
+}
+
+genjs() {
+    javascript=js/$1
+    yui="java -jar yuicompressor-2.4.7/build/yuicompressor-2.4.7.jar"
+    status='[FAIL]'
+    group=../js/$2
+
+    if [ -f $javascript ];
+    then
+        $yui $javascript -o /tmp/jscache/$1
+        echo "/* =====================| $1 - BEGIN |=================== */" >> $group &&
+        cat /tmp/jscache/$1 >> $group &&
+        echo "" >> $group &&
+        echo "/* =====================| $1 - END |=================== */" >> $group &&
+        echo "" >> $group &&
+        status='[ OK ]'
+    fi
+
+    echo "Minifying $1 to $2 $status"
+}
+main
--- controllers/shell.py
+++ controllers/shell.py
@@ -5,15 +5,13 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
         self.response.out.write("""
-        <!DOCTYPE HTML>
-        <html>
-        <head>
-        <title>ayrtonaraujo.net - v0.3.1 Alpha</title>
-        <link href="css/style.css" rel="stylesheet"/>
-        <script type="text/javascript" src="js/shell-0.3.1.js" async="async">
-        </script>
-        </head>
-        <body>
-        </body>
-        </html>
-        """)
+<!DOCTYPE HTML><html><head><title>ayrtonaraujo.net - v0.3.1 Alpha</title>
+<link href="css/style.css" rel="stylesheet"/>
+<script type="text/javascript" src="js/libs-0001.js" async="async"></script>
+<script type="text/javascript" src="js/app-0001.js" async="async"></script>
+</head><body></body></html>""")
+
+
+
+
+
