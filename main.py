99b86e7463095b8539ef387d53ed768ddb7e8c91
Ayrton Araújo <root@ayrtonaraujo.net>
2013-01-29 00:01:00 -0400

    Ported to express.js

--- main.py
+++
@@ -1,6 +0,0 @@
-import webapp2
-
-from controllers import shell
-
-app = webapp2.WSGIApplication([('/', shell.MainPage)],
-                              debug=True)
--- run.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import os
-
-os.system("dev_appserver.py .")
